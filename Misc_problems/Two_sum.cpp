6e6cb0137e45d157c0db2cb440bc85ee90e4bf91
Venkatesh Mishra <bablooven@gmail.com>
2022-02-15 19:49:24 +0530

    Update v4

--- Misc_problems/Two_sum.cpp
+++ Misc_problems/Two_sum.cpp
@@ -3,38 +3,65 @@
 #include <iostream> //For C++
 #include <stdio.h>  //For C
 #include<vector>
+#include<unordered_map>
 
 using namespace std;
 
-vector<int> twoSum(vector<int> &num, int target)
+vector<int> twoSum(vector<int> &nums, int target)
 {
     vector<int> ans;
+    unordered_map<int,int> umap;
+    bool flag1 =false;
 
-    for(int j=0; j<num.size();j++)
+    for(int j=0; j<nums.size();j++)  //Putting the values into an unordered set (Hash Set) //O(n)
     {
-        for(int k=j+1;k<num.size();k++)
+        umap[nums[j]]=j;
+    }
+
+    for(int k =0; k<nums.size();k++)  //O(n)
+    {
+         if(umap.count(target-nums[k])==1 && k!=umap[target-nums[k]])  //O(1)
+         {
+             flag1=true;
+             ans.push_back(k);
+         }
+    }
+
+    if(flag1==true)
+    {
+        for(int l=0;l<nums.size();l++)  //O(n)
         {
-            if(num[j]+num[k]==target)
-            {
-                ans.push_back(j);
-                ans.push_back(k);
-            }
+            if(nums[l]==(target-ans[0]))
+            ans.push_back(l);
         }
     }
+    
+    //Unoptimised solution using O(n^2) in time complexity
+    // for(int j=0; j<nums.size();j++)  //O(n^2)
+    // {
+    //     for(int k=j+1;k<nums.size();k++)
+    //     {
+    //         if(nums[j]+nums[k]==target)
+    //         {
+    //             ans.push_back(j);
+    //             ans.push_back(k);
+    //         }
+    //     }
+    // }
     return ans;
 }
 int main()
 {  
     //target=9
-    int target = 9;
+    int target = 6;
     vector<int> g1;
     
-    //Insertion to the vetor at the end O(1)
-    g1.push_back(1);
-    g1.push_back(2);
+    //Insertion to the vector at the end O(1)
     g1.push_back(3);
+    g1.push_back(2);
     g1.push_back(4);
-    g1.push_back(5);
+    //g1.push_back(4);
+    //g1.push_back(5);
     
     vector<int> ans = twoSum(g1, target);
     cout<<ans[0]<<" "<<ans[1]<<endl;
